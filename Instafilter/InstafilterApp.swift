07f6d8efb372874e8dac9185918e0fd487e714c5
Brandon Glenn <Brandon.Glenn@icloud.com>
2022-01-06 09:19:27 -0700

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Brandon Glenn on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
